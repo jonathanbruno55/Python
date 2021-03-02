e87fc22557e1e9c646953e4a4f768fe591b171b3
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-02 02:36:22 -0600

    Add files via upload

---
+++ Tensor.py
@@ -0,0 +1,53 @@
+import tensorflow as tf # now import the tensorflow module
+print(tf.version) # make sure the version is 2.x
+
+string = tf.Variable("this is a string", tf.string) 
+number = tf.Variable(324, tf.int16)
+floating = tf.Variable(3.567, tf.float64)
+
+rank1_tensor = tf.Variable(["Test"], tf.string) 
+rank2_tensor = tf.Variable([["test", "ok"], ["test", "yes"]], tf.string)
+
+tf.rank(rank2_tensor)
+
+rank2_tensor.shape
+
+tensor1 = tf.ones([1,2,3]) # tf.ones() creates a shape [1,2,3] tensor full of ones
+tensor2 = tf.reshape(tensor1, [2,3,1]) # reshape existing data to shape [2,3,1]
+tensor3 = tf.reshape(tensor2, [3, -1]) # -1 tells the tensor to calculate the size of the dimension in that place
+                                        # this will reshape the tensor to [3,3]
+print(tensor1)
+print(tensor2)
+print(tensor3)
+# Notice the changes in shape
+
+# Creating a 2D tensor
+matrix = [[1,2,3,4,5],
+          [6,7,8,9,10],
+          [11,12,13,14,15],
+          [16,17,18,19,20]]
+
+tensor = tf.Variable(matrix, dtype=tf.int32) 
+print(tf.rank(tensor))
+print(tensor.shape)
+
+three = tensor[0,2]  # selects the 3rd element from the 1st row
+print(three)  # -> 3
+
+row1 = tensor[0]  # selects the first row
+print(row1)
+
+column1 = tensor[:, 0]  # selects the first column
+print(column1)
+
+row_2_and_4 = tensor[1::2]  # selects second and fourth row
+print(row_2_and_4)
+
+column_1_in_row_2_and_3 = tensor[1:3, 0]
+print(column_1_in_row_2_and_3)
+
+
+t = tf.zeros([5,5,5,5])
+print(t)
+t = tf.reshape(t, 625)
+print(t)
